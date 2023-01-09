8e86ccf5ab9a0aa68ba2e0d4b8d7d6dee46efdcb
pk0331 <ec2-user@ip-172-31-10-201.ec2.internal>
2023-01-09 09:41:45 +0000

    GPU

--- modules/private-cluster/cluster.tf
+++ modules/private-cluster/cluster.tf
@@ -345,17 +345,14 @@ resource "google_container_node_pool" "pools" {
       local.node_pools_oauth_scopes[each.value["name"]],
     )
 
-    guest_accelerator = [
-      for guest_accelerator in lookup(each.value, "accelerator_count", 0) > 0 ? [{
-        type               = lookup(each.value, "accelerator_type", "")
+    dynamic "guest_accelerator" {
+      for_each = lookup(each.value, "accelerator_count", null) == null ? []: [1]
+      content {
+        type               = lookup(each.value, "accelerator_type", null) == null ? "nvidia-tesla-t4": each.value["accelerator_type"]
         count              = lookup(each.value, "accelerator_count", 0)
         gpu_partition_size = lookup(each.value, "gpu_partition_size", null)
-        }] : [] : {
-        type               = guest_accelerator["type"]
-        count              = guest_accelerator["count"]
-        gpu_partition_size = guest_accelerator["gpu_partition_size"]
       }
-    ]
+    }
 
     dynamic "workload_metadata_config" {
       for_each = local.cluster_node_metadata_config
